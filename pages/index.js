79bcfee45f0f0fd346f77d488b64cc1122e07ce1
Abdul-Kassem <dinhorkk25@gmail.com>
2026-02-11 18:21:11 +0000

    difica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Eu não sei o que escrever aqui, então escrevi qualquer coisa!!</h1>
+    return <h1>Eu não sei o que escrever aqui, então escrevi o que veio na mente!!!</h1>
 }
 
 export default Home;
