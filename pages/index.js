08f50226869f84516f5f2fa82ed443913f779fc1
Abdul-Kassem <dinhorkk25@gmail.com>
2026-02-20 15:46:35 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Eu não sei o que escrever aqui, então escrevi o que veio na mente!!!</h1>
+function Home() {
+  return (
+    <h1>
+      Eu não sei o que escrever aqui, então escrevi o que veio na mente!!!
+    </h1>
+  );
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
