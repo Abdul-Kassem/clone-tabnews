a2cf59144a19af53095c07858126d7addee149b6
Abdul-Kassem <dinhorkk25@gmail.com>
2026-03-19 18:03:23 +0000

    Adiciona index.js

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,6 @@ function Home() {
   return (
     <h1>
       Eu não sei o que escrever aqui, então escrevi o que veio na mente!!!
-      <h2>Lista Insana</h2>
-      <ol>
-        <li>Insanidade insana</li>
-        <li>Insanidade Meio Insana</li>
-      </ol>
     </h1>
   );
 }
