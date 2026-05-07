e9e1d9fa4d8f7ae782dfea06fa85469bff2fabbc
Abdul-Kassem <dinhorkk25@gmail.com>
2026-05-07 17:51:56 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,29 +6,42 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations deve retornar 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response1.status).toBe(201);
+
+        const responseBody1 = await response1.json();
+
+        expect(Array.isArray(responseBody1)).toBe(true);
+        expect(responseBody1.length).toBeGreaterThan(0);
+        expect(responseBody1[0]).toHaveProperty("path");
+        expect(responseBody1[0]).toHaveProperty("name");
+        expect(responseBody1[0]).toHaveProperty("timestamp");
+      });
+      test("For the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response2.status).toBe(200);
+
+        const responseBody2 = await response2.json();
+
+        expect(Array.isArray(responseBody2)).toBe(true);
+        expect(responseBody2.length).toBe(0);
+      });
+    });
   });
-
-  expect(response1.status).toBe(201);
-
-  const responseBody1 = await response1.json();
-
-  expect(Array.isArray(responseBody1)).toBe(true);
-  expect(responseBody1.length).toBeGreaterThan(0);
-  expect(responseBody1[0]).toHaveProperty("path");
-  expect(responseBody1[0]).toHaveProperty("name");
-  expect(responseBody1[0]).toHaveProperty("timestamp");
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  expect(response2.status).toBe(200);
-
-  const responseBody2 = await response2.json();
-
-  expect(Array.isArray(responseBody2)).toBe(true);
-  expect(responseBody2.length).toBe(0);
 });
