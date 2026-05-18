246864c87c75914c24868c988e8927513f4a8988
Abdul-Kassem <dinhorkk25@gmail.com>
2026-05-18 16:25:29 +0000

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -74,3 +74,24 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Não foi possível encontrar este recurso no sistema.", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action =
+      action || "Verifique se os pârametros enviados na consulta estão certos.";
+    this.status_code = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.status_code,
+    };
+  }
+}
