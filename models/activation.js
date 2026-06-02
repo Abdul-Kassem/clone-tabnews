7e8d1bb9f34f59672ca5cd21f71282f701bd805e
Abdul-Kassem <dinhorkk25@gmail.com>
2026-06-02 13:57:04 -0300

    feat: add `PATCH` `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -1,6 +1,7 @@
 import email from "infra/email.js";
 import database from "infra/database.js";
 import webserver from "infra/webserver.js";
+import user from "models/user.js";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 MINUTOS
 
@@ -75,6 +76,11 @@ async function create(userId) {
   }
 }
 
+async function activateUserByUserId(userId) {
+  const activatedUser = await user.setFeatures(userId, ["create:session"]);
+  return activatedUser;
+}
+
 async function sendEmailToUser(user, activationToken) {
   await email.send({
     from: "FinTab <contato@abdulrkk.com.br>",
@@ -89,11 +95,37 @@ Equipe Fintab`,
   });
 }
 
+async function markTokenAsUsed(activationTokenId) {
+  const usedActivationToken = await runUpdateQuery(activationTokenId);
+  return usedActivationToken;
+
+  async function runUpdateQuery(activationTokenId) {
+    const results = await database.query({
+      text: `
+        UPDATE
+          user_activation_tokens
+        SET
+          used_at = timezone('utc', now()),
+          updated_at = timezone('utc', now())
+        WHERE
+          id = $1
+        RETURNING
+          *
+      ;`,
+      values: [activationTokenId],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const activation = {
   sendEmailToUser,
   create,
   findOneByUserId,
   findValidTokenById,
+  markTokenAsUsed,
+  activateUserByUserId,
 };
 
 export default activation;
